a88b09ade928b7fe0d91fbb17378f5954e913d03
Ralph Borcherds <ralph@mrbconsulting.biz>
2018-01-30 22:18:56 -0800

    LocalSorage Example

---
+++ localstorage/json.js
@@ -0,0 +1,131 @@
+function readWeatherFile(url, divId, isJSON) {
+    /**********************************************
+     * This function reads a JSON input file from the server.
+     * input:   JSON Data File on the server
+     * Processing:  Takes this data and converts it to a JavaScript array.
+     *      It then finds the row number selected by the user and displays that data
+     * Output:   JavaScript Array of data.
+     *************************************************/
+
+    // Create the object to read the file data
+    var xmlhttp = new XMLHttpRequest();
+    // Setup the object to only run when the file has finished being loaded
+    xmlhttp.onreadystatechange =
+        //	This code is not executed immedidaitely. It is call later when the server starts to respond.
+        function() {
+            // This next line checks to make sure that the file has finished being read and that it was read correctly.
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+            //	This method is going to serve a several kinds of requests
+            //	we are taking the 2nd and 3rd fields from the onclick request and passing them to the worked function
+                if (url.includes("http") == true || url.includes("https") == true) {
+                    doSomethingWithTheResponse2(xmlhttp.responseText, divId, isJSON);
+                } else {
+                    doSomethingWithTheResponse(xmlhttp.responseText, divId, isJSON);
+                }
+                // This is code to help debug the file reading process.
+                //	else	// this will show us what is happening before the data arrives
+                //		document.getElementById( 'serverState' ).innerHTML += "Ready State: " + xmlhttp.readyState + "  Status: " + xmlhttp.status + "<BR>";
+        }
+        //	This is where we make our request to the server. We pass along the URL for the file we want
+        //	You can see the name of the file we are requesting in the about HTML code.
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function doSomethingWithTheResponse(response, divId, isJSON) {
+    /**********************************************
+     * This function takes the file that was read and exicutes what we want done with it
+     * input:   JSON Data File, 
+     *       The DIV ID where the out needs to be written, 
+     *       is this file a JSON file
+     * Processing:  Takes this data and converts it to a JavaScript array.
+     *      It then finds the row number selected by the user and displays that data
+     * Output:   JavaScript Array of data.
+     *************************************************/
+    var responseText = (isJSON) ? JSON.parse(response) : response;
+
+    //	Now we will take the text and do something with it
+    if (isJSON) {
+        var i = 0; // Set counter variable
+        // Get the record number that the user wants to display and subtract 1
+        // JavaScript arrays start at 0 but the data starts at 1
+        var rownum = document.getElementById("rownum").value - 1;
+
+        // Get the row number from the first level array and create a new array with just that data
+        var data = responseText[rownum];
+        // Get the names of the variable names in the string
+        var names = Object.keys(data)
+            // create a variable to output the data to the HTML page
+        var responses = "";
+
+        // Loop through the variables in the array and create the output
+        for (i; i < names.length; i++) {
+            // names[i] is the named element name
+            // data[names[i]] is the value of the elemet.  
+            // This is calling the elements by name and not by reference number
+            responses += names[i] + ": " + data[names[i]] + "<br>";
+        }
+        // This writes out the row selected to the screen
+        document.getElementById(divId + "2").innerHTML = "Just the text<br>" + responses;
+    } else {
+        // If the file is not a JSON file then the entire contents are written out.
+        document.getElementById(divId).innerHTML = responseText;
+    }
+}
+
+function doSomethingWithTheResponse2(response, divId, isJSON) {
+    /**********************************************
+     * This function takes the file that was read and exicutes what we want done with it
+     * input:   JSON Data File, 
+     *       The DIV ID where the out needs to be written, 
+     *       is this file a JSON file
+     * Processing:  Takes this data and converts it to a JavaScript array.
+     *      It then finds the row number selected by the user and displays that data
+     * Output:   JavaScript Array of data.
+     *************************************************/
+    var responseText = (isJSON) ? JSON.parse(response) : response;
+
+    //	Now we will take the text and do something with it
+    if (isJSON) {
+        var i = 0; // Set counter variable
+        var x = 0;
+        var test = responseText.current_observation;
+        if (test === undefined) {
+            var data = responseText.forecast.simpleforecast.forecastday;
+
+            for (index in data) {
+                localStorage.setItem('day' + index, data[index]['date']['weekday_short']);
+                localStorage.setItem('day' + index + '-high', data[index]['high']['fahrenheit']);
+                localStorage.setItem('day' + index + '-low', data[index]['low']['fahrenheit']);
+            }
+            localStorage.setItem('todayhigh', data['0']['high']['fahrenheit']);
+            localStorage.setItem('todaylow', data['0']['low']['fahrenheit']);
+            localStorage.setItem('percip', data['0']['pop']);
+            localStorage.setItem('maxwind', data['0']['maxwind']['mph'] + " mph");
+
+
+        } else {
+
+            var data = responseText.current_observation;
+            var location = data.display_location;
+            var names = Object.keys(data);
+            var locnames = Object.keys(location);
+
+            // Loop through the variables in the array and create the output
+            for (i; i < names.length; i++) {
+                localStorage.setItem(names[i], data[names[i]]);
+            }
+            for (x; x < locnames.length; x++) {
+
+                localStorage.setItem(locnames[x], location[locnames[x]]);
+            }
+        }
+        // Get the names of the variable names in the string
+
+        // This writes out the row selected to the screen
+        // document.getElementById(divId + "2").innerHTML = "Just the text<br>" + responses;
+    } else {
+        // If the file is not a JSON file then the entire contents are written out.
+        document.getElementById(divId).innerHTML = responseText;
+    }
+}
---
+++ localstorage/localStorageAPI.js
@@ -0,0 +1,102 @@
+// Local Storage API
+var k = "";
+var v = "";
+var storeArry = new Object();
+
+function doFirst() {
+    var fileName = location.pathname.split("/").slice(-1)
+    if (fileName == "localstorage.html") {
+        var saveButton = document.getElementById("saveButton");
+        var remButton = document.getElementById("remButton");
+        var clrButton = document.getElementById("clrButton");
+        saveButton.addEventListener("click", saveInfo, false);
+        remButton.addEventListener("click", removeInfo, false);
+        clrButton.addEventListener("click", clearLocalStore, false);
+        display();
+    }
+}
+
+function saveInfo() {
+    k = document.getElementById("person").value;
+    v = document.getElementById("age").value;
+    localStorage.setItem(k, v);
+    display();
+    document.getElementById("person").value = "";
+    document.getElementById("age").value = "";
+}
+
+function arrayStore() {
+    var rightbox = document.getElementById("rightbox");
+    storeArry = {};
+    rightbox.innerHTML = "";
+    for (i = 0; i < localStorage.length; i++) {
+        var a = localStorage.key(i);
+        var b = localStorage.getItem(a);
+        storeArry[a] = b;
+    }
+}
+
+function display() {
+    arrayStore();
+    rightbox.innerHTML = "<strong>Local Storage Items:</strong><br />";
+    for (var j in storeArry) {
+        rightbox.innerHTML += j + ": " + storeArry[j] + "<br />";
+    }
+}
+
+function removeInfo() {
+    var r = document.getElementById("person").value;
+    localStorage.removeItem(r);
+    display();
+}
+
+function clearLocalStore() {
+    localStorage.clear();
+    display();
+}
+
+function arrayStoreWeather() {
+    storeArry = {};
+    for (i = 0; i < localStorage.length; i++) {
+        var a = localStorage.key(i);
+        var b = localStorage.getItem(a);
+        storeArry[a] = b;
+    }
+}
+
+function writeLocalWeather() {
+    arrayStoreWeather();
+    if (arrayStoreWeather.length != 0) {
+    document.getElementById('cityname').innerHTML = storeArry['full'];
+    document.getElementById('maxwindspeed').innerHTML = storeArry['maxwind'];
+    document.getElementById('summary2').innerHTML = storeArry['temp_f'] + " F";
+    document.getElementById('hightemp').innerHTML = storeArry['todayhigh'] + " F";
+    document.getElementById('lowtemp').innerHTML = storeArry['todaylow'] + " F";
+    document.getElementById('chancepercperc').innerHTML = storeArry['percip'] + " %";
+    document.getElementById('day0').innerHTML = storeArry['day0'];
+    document.getElementById('day1').innerHTML = storeArry['day1'];
+    document.getElementById('day2').innerHTML = storeArry['day2'];
+    document.getElementById('day3').innerHTML = storeArry['day3'];
+    document.getElementById('day4').innerHTML = storeArry['day4'];
+    document.getElementById('day5').innerHTML = storeArry['day5'];
+    document.getElementById('day6').innerHTML = storeArry['day6'];
+    document.getElementById('day7').innerHTML = storeArry['day7'];
+    document.getElementById('day8').innerHTML = storeArry['day8'];
+    document.getElementById('day9').innerHTML = storeArry['day9'];
+    document.getElementById('day0temp').innerHTML = storeArry['day0-high'];
+    document.getElementById('day1temp').innerHTML = storeArry['day1-high'];
+    document.getElementById('day2temp').innerHTML = storeArry['day2-high'];
+    document.getElementById('day3temp').innerHTML = storeArry['day3-high'];
+    document.getElementById('day4temp').innerHTML = storeArry['day4-high'];
+    document.getElementById('day5temp').innerHTML = storeArry['day5-high'];
+    document.getElementById('day6temp').innerHTML = storeArry['day6-high'];
+    document.getElementById('day7temp').innerHTML = storeArry['day7-high'];
+    document.getElementById('day8temp').innerHTML = storeArry['day8-high'];
+    document.getElementById('day9temp').innerHTML = storeArry['day9-high'];
+} else {
+    document.getElementById('forecast').innerHTML = "<p>There is no weather data to display. Please go back one page and get the weather info first</p>";
+}
+}
+
+
+window.addEventListener("load", doFirst, false);
